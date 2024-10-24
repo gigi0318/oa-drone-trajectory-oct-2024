ce2e9395f3b3f15c5b2c84d45990780ab63e02d3
gigi0318 <2giagill@gmail.com>
2024-10-24 09:16:13 -0400

    UpdatedGIAdata_model.py

--- src/data_model.py
+++ src/data_model.py
@@ -10,7 +10,20 @@ class Camera:
     - https://github.com/colmap/colmap/blob/3f75f71310fdec803ab06be84a16cee5032d8e0d/src/colmap/sensor/models.h#L220
     - https://en.wikipedia.org/wiki/Pinhole_camera_model
     """
-    pass
+    fx: float # Focal length along the x axis (in pixels)
+    fy: float # Focal length along the y axis (in pixels)
+    cx: float # Optical center of the image along the x axis (in pixels)
+    cy: float # Optical center of the image along the y axis (in pixels)
+    sensor_size_x_mm: float # Size of the sensor along the x axis (in mm)
+    sensor_size_y_mm: float # Size of the sensor along the y axis (in mm)
+    image_size_x: int # Number of pixels in the image along the x axis
+    image_size_y: int # Number of pixels in the image along the y axis
+
+    def __repr__(self):
+        return (f"Camera(fx={self.fx}, fy={self.fy}, cx={self.cx}, cy={self.cy}, "
+    f"sensor_size_x_mm={self.sensor_size_x_mm}, "
+    f"sensor_size_y_mm={self.sensor_size_y_mm}, "
+    f"image_size_x={self.image_size_x}, image_size_y={self.image_size_y})")
 
 @dataclass
 class DatasetSpec:
